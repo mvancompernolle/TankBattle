575b95e43f5cf94dd0905b355f8f5a777cbd2efe
Terry Nguyen <terreh@terrehbyte.com>
2016-04-06 15:12:12 -0700

    Cleaned up and organized example client code

--- TankClientLib/TankBattle-client-dyad-sfw/main.cpp
+++ TankClientLib/TankBattle-client-dyad-sfw/main.cpp
@@ -1,19 +1,12 @@
 #include <iostream>
-#include <random>
-#include <time.h>
 
 #include "TankBattleNet.h"
-
 #include "sfwdraw.h"
 #undef NONE
 
-int     myPlayerID = -1;
+int myPlayerID = -1;
 
-bool    isConnected = false;   
-bool    isWaiting = false;
-bool    isProvisioned = false;
-
-bool    inputPressed()
+bool inputPressed()
 {
     for (unsigned int i = 0; i < 150; ++i)
     {
@@ -59,8 +52,6 @@ int main(int argc, char** argv)
     // Boot-up sequence
     std::cout << "TankBattleClient v" << CLIENT_MAJOR_VERSION << "." << CLIENT_MINOR_VERSION << "\n";
 
-    srand(time(NULL));
-
     // initialize networking
     if (serverIPAddress[0] == '\0')
     {
@@ -101,57 +92,26 @@ int main(int argc, char** argv)
         ex.messageLength = msgSize;    // TODO: support for dynamic message length
         ex.playerID = myPlayerID;
 
-        if (ex.playerID == -1)
-            isWaiting = true;
-
         // poll for input
         if (inputPressed())
         {
             // tank actions
-            if (sfw::getKey(TANK_FWRD))
-            {
-                ex.tankMove = TankMovementOptions::FWRD;
-            }
-            else if (sfw::getKey(TANK_BACK))
-            {
-                ex.tankMove = TankMovementOptions::BACK;
-            }
-            else if (sfw::getKey(TANK_LEFT))
-            {
-                ex.tankMove = TankMovementOptions::LEFT;
-            }
-            else if (sfw::getKey(TANK_RIGT))
-            {
-                ex.tankMove = TankMovementOptions::RIGHT;
-            }
-            else
-            {
-                ex.tankMove = TankMovementOptions::HALT;
-            }
+            ex.tankMove =   sfw::getKey(TANK_FWRD) ? TankMovementOptions::FWRD :
+                            sfw::getKey(TANK_BACK) ? TankMovementOptions::BACK :
+                            sfw::getKey(TANK_LEFT) ? TankMovementOptions::LEFT :
+                            sfw::getKey(TANK_RIGT) ? TankMovementOptions::RIGHT :
+                            TankMovementOptions::HALT;
 
-            if (sfw::getKey(CANN_LEFT))
-            {
-                ex.cannonMove = CannonMovementOptions::LEFT;
-            }
-            else if (sfw::getKey(CANN_RIGT))
-            {
-                ex.cannonMove = CannonMovementOptions::RIGHT;
-            }
-            else
-            {
-                ex.cannonMove = CannonMovementOptions::HALT;
-            }
+            ex.cannonMove = sfw::getKey(CANN_LEFT) ? CannonMovementOptions::LEFT :
+                            sfw::getKey(CANN_RIGT) ? CannonMovementOptions::RIGHT :
+                            CannonMovementOptions::HALT;
 
-            if (sfw::getKey(TANK_FIRE))
-            {
-                ex.fireWish = true;
-            }
+            ex.fireWish = sfw::getKey(TANK_FIRE);
 
             // game actions
             if (sfw::getKey(GAME_QUIT))
             {
                 ex.msg = tankBattleMessage::QUIT;
-                break;
             }
         }
 
