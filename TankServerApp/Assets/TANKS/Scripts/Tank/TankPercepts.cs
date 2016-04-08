124c9e52b6ff026e703f55330ccd6e78fd66abe9
Terry Nguyen <terreh@terrehbyte.com>
2016-04-07 17:04:10 -0700

    Added LOS check

--- TankServerApp/Assets/TANKS/Scripts/Tank/TankPercepts.cs
+++ TankServerApp/Assets/TANKS/Scripts/Tank/TankPercepts.cs
@@ -3,8 +3,17 @@ using System.Collections;
 
 public class TankPercepts : MonoBehaviour
 {
+    //[HideInInspector]
     public Vector3 lastKnownPosition;
+    //[HideInInspector]
     public Vector3 lastKnownDirection;
+
+    public float VisionRadius;
+
+    [SerializeField]
+    private Transform eyePoint;
+    [SerializeField]
+    private float eyeFOV = 90f;
 
     void OnEnable()
     {
@@ -15,6 +24,30 @@ public class TankPercepts : MonoBehaviour
     {
         lastKnownDirection = Vector3.zero;
         lastKnownDirection = Vector3.zero;
+    }
+
+    void FixedUpdate()
+    {
+        var radar = Physics.OverlapSphere(transform.position, VisionRadius, ~(LayerMask.NameToLayer("Players")));
+        foreach(var ping in radar)
+        {
+            if(ping.gameObject != gameObject)
+            {
+                var direction = (ping.transform.position - transform.position).normalized;
+
+                if(Vector3.Angle(eyePoint.forward, direction) < eyeFOV)
+                {
+                    RaycastHit hit;
+                    if(Physics.Raycast(transform.position, direction, out hit))
+                    {
+                        if (hit.collider == ping)
+                        {
+                            lastKnownPosition = hit.transform.position;
+                        }
+                    }
+                }
+            }
+        }
     }
 
     void OnDrawGizmos()
